58dfc36a2566c6588a6c4e17433857b07af70dc7
Renaud heluin <renaud.heluin@novagaia.fr>
2026-02-06 11:24:43 +0100

    fix: add base for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 const SITE_URL = process.env.SITE_URL || "http://localhost:4321";
+const REPO_NAME = process.env.REPO_NAME || "2024-astro-kenober";
 const tina = ({ directiveName = "tina" } = {}) => ({
   name: "tina-cms",
   hooks: {
@@ -22,6 +23,7 @@ const tina = ({ directiveName = "tina" } = {}) => ({
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+  base: `/${REPO_NAME}`,
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
